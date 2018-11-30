282bdb2717aa9541f5861e6d25e8ca68cb5c2239
Ubuntu <vagrant@Base-Box>
2018-11-30 22:10:24 +0000

    Generated necessary controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/show'
+  get 'orders/new'
+  get 'carts/show'
+  get 'products/index'
+  get 'products/new'
+  get 'products/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
